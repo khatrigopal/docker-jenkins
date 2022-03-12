8911700ecbcfdad95e37b711a6d9decc549ec84f
Gopal Khatri <khatri.gopal@gmail.com>
2022-03-12 09:17:34 +0000

    added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
 	response.writeHead(200);
-	response.end("This is  GCP Cloud Testingn!");
+	response.end("This is  GCP Cloud Testing!");
 }
 
 var www = http.createServer(handleRequest);
