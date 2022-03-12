d9394081a9d2eec75985475a1bd2ee339a1ef36e
Gopal Khatri <khatri.gopal@gmail.com>
2022-03-12 09:48:08 +0000

    added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
 	response.writeHead(200);
-	response.end("This is  GCP Cloud Testing for Jenkins and Docker!");
+	response.end("This is  GCP Cloud Testing for Jenkins and Docker push!");
 }
 
 var www = http.createServer(handleRequest);
